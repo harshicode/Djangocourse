724ccc2f9fead19b2866cb5b726fef8d90e10ca4
Harshitha <harshithan055@gmail.com>
2022-07-18 12:57:02 -0400

    Final Changes

--- movies/settings.py
+++ movies/settings.py
@@ -23,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kyd&=2&_+xxw3ws+^aa$-3s231dgs$i2ph85wymcqy$+8rxr!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'movies',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'E:\github\movies\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
